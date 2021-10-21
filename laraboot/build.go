55e1e429ab700ec66ad61d4a40ba8ca7d250285c
Oscar <fu.wire@gmail.com>
2021-10-20 20:56:58 -0500

    wip(commander-commands): wip

--- laraboot/build.go
+++ laraboot/build.go
@@ -5,6 +5,7 @@ import (
 	_ "embed" // required
 	"errors"
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/bitfield/script"
@@ -20,14 +21,12 @@ var commanderYml string
 func Build(logger shared.LogEmitter) packit.BuildFunc {
 	return func(context packit.BuildContext) (packit.BuildResult, error) {
 		logger.Title("%s %s", context.BuildpackInfo.Name, context.BuildpackInfo.Version)
-		logger.Process("Reading laraboot.json")
 		thisLayer, blueprintGenErr := context.Layers.Get("laraboot-commander")
 		if blueprintGenErr != nil {
 			return packit.BuildResult{}, blueprintGenErr
 		}
 
 		_, errDecoding := shared.NewFromFile(filepath.Join(context.WorkingDir, "laraboot.json"))
-
 		if errDecoding != nil {
 			fmt.Printf("	--> An error occurred while parsing laraboot file: '%s'", errDecoding)
 		}
@@ -48,23 +47,25 @@ func Build(logger shared.LogEmitter) packit.BuildFunc {
 		}
 
 		unmarshallErr := yaml.Unmarshal([]byte(commanderYml), &m)
-
 		if unmarshallErr != nil {
-			fmt.Printf("unmarshallErr: %v", unmarshallErr)
 			return packit.BuildResult{}, unmarshallErr
 		}
 
 		commandsLen := len(m.Commander.Commands)
 		for k, v := range m.Commander.Commands {
-			file := fmt.Sprintf("%s/command-%d", thisLayer.Path, k)
+			fileName := fmt.Sprintf("%s/command-%d", thisLayer.Path, k)
 			body := fmt.Sprintf("#!/usr/bin/env bash \n %s", v.Run)
 			logger.Process("Running command [%d/%d]: %s", k+1, commandsLen, v.Name)
-			writeFile, err := script.Echo(body).WriteFile(file)
+			_, err := os.Create(fileName)
+			if err != nil {
+				return packit.BuildResult{}, err
+			}
+			writeFile, err := script.Echo(body).WriteFile(fileName)
 			fmt.Println(writeFile)
 			if err != nil {
 				return packit.BuildResult{}, err
 			}
-			p := script.Exec(fmt.Sprintf("bash -c '%s'", file))
+			p := script.Exec(fmt.Sprintf("bash -c '%s'", fileName))
 			output, _ := p.String()
 			fmt.Println(output)
 			var exit = p.ExitStatus()
