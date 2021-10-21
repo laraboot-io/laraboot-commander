2620b0a549bf49d7d3a524ea89c5cecece8af8cb
Oscar <fu.wire@gmail.com>
2021-10-20 21:59:39 -0500

    wip(commander-commands): simplify

--- test/buildpack-test.sh
+++ test/buildpack-test.sh
@@ -15,6 +15,9 @@ echo "BUILDPACKS_ROOT=$BUILDPACKS_ROOT"
 mkdir sample-app
 cp -R examples/01-commands/ sample-app
 
+pwd
+ls -ltah sample-app
+
 pack build app-name --path sample-app \
   --buildpack paketo-buildpacks/php-dist \
   --buildpack paketo-buildpacks/php-composer \
