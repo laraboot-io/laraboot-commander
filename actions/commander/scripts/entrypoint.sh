0075669c838862e75a6096d3c22f50816bb3ee02
Oscar <fu.wire@gmail.com>
2021-10-22 19:03:19 -0500

    - pack as CNB file -

--- actions/commander/scripts/entrypoint.sh
+++ actions/commander/scripts/entrypoint.sh
@@ -2,4 +2,12 @@
 set -eu
 set -o pipefail
 
-pack "$@"
+WORKSPACE_DIR="${WORKSPACE_DIR:/github/workspace}"
+
+pwd
+
+go build -ldflags="-s -w" -o ./bin/detect ./cmd/detect/main.go &&
+go build -ldflags="-s -w" -o ./bin/build ./cmd/build/main.go &&
+pack buildpack package "$WORKSPACE_DIR/dist/laraboot-commander.cnb" --config ./package.toml --format file
+
+exit 0
